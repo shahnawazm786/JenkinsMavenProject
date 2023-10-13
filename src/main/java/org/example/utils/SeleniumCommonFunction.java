ef9855ff92b4815412a09ba50af0047f0d8b7eb0
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-10-13 08:12:13 +0530

    jenkins integeration

--- src/main/java/org/example/utils/SeleniumCommonFunction.java
+++ src/main/java/org/example/utils/SeleniumCommonFunction.java
@@ -3,10 +3,16 @@ package org.example.utils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class SeleniumCommonFunction {
     public static WebElement findElement(WebDriver driver, By locator){
         return driver.findElement(locator);
     }
+    public static WebElement findElement(WebDriver driver, WebElement element){
+        return element;
+    }
+    public static void waitForElementVisiblity(WebDriver driver){
 
+    }
 }
