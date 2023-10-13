c533ca1cc4620c75823d8c34093d4bf37f9d5a7a
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-10-13 09:15:35 +0530

    jenkins integeration

--- src/main/java/org/example/utils/SeleniumCommonFunction.java
+++ src/main/java/org/example/utils/SeleniumCommonFunction.java
@@ -9,6 +9,9 @@ import java.time.Duration;
 
 public class SeleniumCommonFunction {
     static WebDriverWait wait;
+    public static void pageImplicitWait(WebDriver driver){
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(ICommonValue.PAGE_TIME_OUT));
+    }
     public static WebElement findElement(WebDriver driver, By locator){
         return driver.findElement(locator);
     }
@@ -16,13 +19,20 @@ public class SeleniumCommonFunction {
         return element;
     }
     public static void waitForElementVisibility(WebDriver driver,WebElement locator){
-        wait=new WebDriverWait(driver, Duration.ofSeconds(ICommonValue.TIME_OUT));
+        wait=new WebDriverWait(driver, Duration.ofSeconds(ICommonValue.ELEMENT_TIME_OUT));
         wait.ignoring(ElementClickInterceptedException.class, ElementNotInteractableException.class)
                 .until(ExpectedConditions.visibilityOf(locator));
 
     }
     public static boolean isElementDisplayed(WebDriver driver,By element){
-        return new WebDriverWait(driver,Duration.ofSeconds(ICommonValue.TIME_OUT))
+        return new WebDriverWait(driver,Duration.ofSeconds(ICommonValue.ELEMENT_TIME_OUT))
                 .until((WebDriver d)->d.findElement(element).isDisplayed());
+    }
+    public static boolean isElementDisplayed(WebDriver driver,WebElement element){
+        return new WebDriverWait(driver,Duration.ofSeconds(ICommonValue.ELEMENT_TIME_OUT))
+                .until((WebDriver d)->element.isDisplayed());
+    }
+    public static  void WebPageMaximize(WebDriver driver){
+        driver.manage().window().maximize();
     }
 }
