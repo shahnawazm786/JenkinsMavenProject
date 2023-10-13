a2b24e4cfc78a284f3eeb9b4cb9e7bc462c04d83
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-10-13 09:15:21 +0530

    jenkins integeration

--- src/main/java/org/example/utils/ICommonValue.java
+++ src/main/java/org/example/utils/ICommonValue.java
@@ -1,5 +1,8 @@
 package org.example.utils;
 
 public interface ICommonValue {
-    static final long TIME_OUT=20;
+    static final long PAGE_TIME_OUT=30;
+    static final long ELEMENT_TIME_OUT=20;
+    static final String CHROME_PATH="C:\\Program Files\\Google\\Chrome\\Application\\new_chrome.exe";
+    static final String WEB_URL="https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
 }
