e5d61fade466c9d76640ff84611701b667ba10eb
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-10-14 20:55:43 +0530

    jenkins integeration

--- src/test/java/steps/HomePagedefs.java
+++ src/test/java/steps/HomePagedefs.java
@@ -1,7 +1,9 @@
 package steps;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.example.pages.OrangeHRMSHomePage;
 import org.openqa.selenium.WebDriver;
 
@@ -18,5 +20,29 @@ public class HomePagedefs {
     public void verify_the_login_name() {
         System.out.println("Verified the login tag");
         orangeHRMSHomePage.verifyLoginNameDisplayed(driver);
+    }
+
+    @When("I verify the username label")
+    public void iVerifyTheUsernameLabel() {
+    }
+
+    @And("I verify the username text box")
+    public void iVerifyTheUsernameTextBox() {
+    }
+
+    @When("I verify the password label")
+    public void iVerifyThePasswordLabel() {
+    }
+
+    @And("I verify the password text box")
+    public void iVerifyThePasswordTextBox() {
+    }
+
+    @And("I verify the login button")
+    public void iVerifyTheLoginButton() {
+    }
+
+    @When("I verify the forgot password link")
+    public void iVerifyTheForgotPasswordLink() {
     }
 }
