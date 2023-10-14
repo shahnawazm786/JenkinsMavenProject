6cac5b8069c182c0ebada3232540b2ac84aae046
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-10-14 21:17:28 +0530

    jenkins integeration

--- src/main/java/org/example/pages/ILocator.java
+++ src/main/java/org/example/pages/ILocator.java
@@ -8,4 +8,8 @@ public interface ILocator {
     String passwordTextBoxByName="password";
     String loginButtonByXpath="//button[@type='submit']";
     String forgotPasswordLinkByXpath="//p[text()='Forgot your password? ']";
+    String iconLinkedInByXpath="//div[@class='orangehrm-login-footer-sm']/child::a[1]";
+    String iconFaceBookByXpath="//div[@class='orangehrm-login-footer-sm']/child::a[2]";
+    String iconTwitterByXpath="//div[@class='orangehrm-login-footer-sm']/child::a[3]";
+    String iconYoutubeByXpath="//div[@class='orangehrm-login-footer-sm']/child::a[4]";
 }
