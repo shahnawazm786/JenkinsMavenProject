e39fe3008a7e6e2b3265344a87d19d13ae99a4ce
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-10-14 20:55:57 +0530

    jenkins integeration

--- src/main/java/org/example/pages/OrangeHRMSHomePage.java
+++ src/main/java/org/example/pages/OrangeHRMSHomePage.java
@@ -2,6 +2,7 @@ package org.example.pages;
 
 import org.example.utils.SeleniumCommonFunction;
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -19,6 +20,13 @@ public class OrangeHRMSHomePage {
     private WebElement loginH5;
     @FindBy(how=How.XPATH,using="(//p[@class='oxd-text oxd-text--p'])[1]")
     private WebElement pLoginName;
+    By userNameLabel=By.xpath(ILocator.userNameLabelByXpath);
+    By userNameTextBox=By.name(ILocator.userNameTextBoxByName);
+    By passwordLabel=By.xpath(ILocator.passwordLabelByXpath);
+    By passwordTextBox=By.name(ILocator.passwordTextBoxByName);
+    By loginButton=By.xpath(ILocator.loginButtonByXpath);
+    By forgotPasswordLink=By.xpath(ILocator.forgotPasswordLinkByXpath);
+
     public void verifyTagName(WebDriver driver){
         Assert.assertTrue(SeleniumCommonFunction.isElementDisplayed(driver,loginH5));
         //SeleniumCommonFunction.findElement(driver, loginH5);
@@ -27,4 +35,17 @@ public class OrangeHRMSHomePage {
         boolean isDisplayed = SeleniumCommonFunction.isElementDisplayed(driver, pLoginName);
         Assert.assertTrue(isDisplayed);
     }
+    public void verifyUserNameLabelDisplayed(WebDriver driver){
+        Assert.assertTrue(SeleniumCommonFunction.isElementDisplayed(driver,userNameLabel));
+    }
+    public void verifyUserNameTextDisplayed(WebDriver driver){
+        Assert.assertTrue(SeleniumCommonFunction.isElementDisplayed(driver,userNameTextBox));
+    }
+    public void verifyPasswordLabel(WebDriver driver){
+        Assert.assertTrue(SeleniumCommonFunction.isElementDisplayed(driver,passwordLabel));
+    }
+    public void verifyPasswordTextBox(WebDriver driver){
+        Assert.assertTrue(SeleniumCommonFunction.isElementDisplayed(driver,passwordTextBox));
+    }
+
 }
