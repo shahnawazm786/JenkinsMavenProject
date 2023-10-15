e912c14d42e4210ace7760a3bf6c1df72ce2d83e
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-10-15 10:39:18 +0530

    jenkins integeration

---
+++ src/test/java/steps/LoginSteps.java
@@ -0,0 +1,24 @@
+package steps;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginSteps {
+    @When("user enter username {string} and password {string}")
+    public void userEnterUsernameAdminAndPasswordAdmin(String username,String password) {
+    }
+
+    @Then("user should be able to login successfully")
+    public void userShouldBeAbleToLoginSuccessfully() {
+        
+    }
+
+    @When("user enter username as {string} and password as {string}")
+    public void userEnterUsernameAsAndPasswordAs(String arg0, String arg1) {
+        
+    }
+
+    @Then("user should be able to see the error message {string}")
+    public void userShouldBeAbleToSeeTheErrorMessage(String arg0) {
+    }
+}
