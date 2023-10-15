4c4154f970710789af687a26c863962a829ce5ab
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-10-15 10:39:12 +0530

    jenkins integeration

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -3,12 +3,9 @@ package steps;
 import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
-import org.example.utils.ICommonValue;
-import org.example.utils.SeleniumCommonFunction;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import io.cucumber.java.After;
 public class Hooks {
     static WebDriver driver;
@@ -23,12 +20,12 @@ public class Hooks {
             //driver.quit();
         }
     }
-    @AfterStep
+    /*@After
     public void addScreenShot(Scenario scenario){
         if(scenario.isFailed()){
             final byte[] screenshot=((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
 
         }
-    }
+    }*/
 
 }
