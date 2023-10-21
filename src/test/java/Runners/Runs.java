2d7d69ac5f1714697a9339005dbe0f5b7c3160d0
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-10-21 21:06:17 +0530

    jenkins integeration

--- src/test/java/Runners/Runs.java
+++ src/test/java/Runners/Runs.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "steps",
-        tags = "@login",
+        tags = "@invalidcredential",
         plugin = {"pretty","json:target/maven-json-file/orangehrms.json"}
 )
 public class Runs {
