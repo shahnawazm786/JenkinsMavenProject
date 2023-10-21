839955cbde304a04873bd1c75e74ac9e5b7225f6
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-10-21 12:09:05 +0530

    jenkins integeration

---
+++ src/main/java/org/example/pages/OrnageHRMSLoginPage.java
@@ -0,0 +1,45 @@
+package org.example.pages;
+
+import org.example.utils.SeleniumCommonFunction;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+public class OrnageHRMSLoginPage {
+    WebDriver driver;
+    public OrnageHRMSLoginPage(WebDriver driver){
+        this.driver=driver;
+        PageFactory.initElements(driver,this);
+    }
+    // Locator
+    By userNameTextBoxByName =By.name(ILocator.userNameTextBoxByName);
+    By passwordTextBoxByName=By.name(ILocator.passwordTextBoxByName);
+    By loginButtonByXpath=By.xpath(ILocator.loginButtonByXpath);
+    By errorMessageLabelXpath=By.xpath(ILocator.errorMessageLabelXpath);
+
+    public void enterDataInUserNameTextBox(){
+        SeleniumCommonFunction.waitForElementVisibility(driver, userNameTextBoxByName);
+        SeleniumCommonFunction.SendKeys(driver,userNameTextBoxByName,"Admin");
+    }
+
+    public void enterDataInUserNameTextBox(String userName){
+        SeleniumCommonFunction.waitForElementVisibility(driver, userNameTextBoxByName);
+        SeleniumCommonFunction.SendKeys(driver,userNameTextBoxByName,userName);
+    }
+    public void enterDataInPasswordTextBox(){
+        SeleniumCommonFunction.waitForElementVisibility(driver, passwordTextBoxByName);
+        SeleniumCommonFunction.SendKeys(driver,passwordTextBoxByName,"admin123");
+    }
+    public void enterDataInPasswordTextBox(String password){
+        SeleniumCommonFunction.waitForElementVisibility(driver, passwordTextBoxByName);
+        SeleniumCommonFunction.SendKeys(driver,passwordTextBoxByName,password);
+    }
+    public void verifyErrorMessageLabelDisplayed(){
+        Assert.assertTrue(SeleniumCommonFunction.isElementDisplayed(driver,errorMessageLabelXpath));
+    }
+    public void verifyErrorMessageforInvalidCredential(String errorMessagge){
+
+    }
+}
